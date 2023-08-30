6585534abdba3d0ba8a39d42265611ee3311768f
Leomi95 <leomills2004@gmail.com>
2023-08-30 18:17:41 -0300

    php

---
+++ registro-php/index.php
@@ -0,0 +1,11 @@
+
+<?php
+// Conexión a la base de datos
+$conexion = new mysqli("localhost", "angel", "Kurokonoba95", "mi_base_de_datos") or die("error al conectart");
+if($conexion){
+    echo "conexion exitosa";
+}
+
+// Verificar la conexión
+$conexion->close();
+?>
