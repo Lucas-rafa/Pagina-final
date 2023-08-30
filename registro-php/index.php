5593128fa14e6fc7237aab346b3b1213fafd27a3
Leomi95 <leomills2004@gmail.com>
2023-08-30 19:07:53 -0300

    lindo

--- registro-php/index.php
+++ registro-php/index.php
@@ -1,9 +1,5 @@
 <?php
 // Obtener los datos del formulario de registro
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$usuario = $_POST["usuario"];
-$contrasena = $_POST["contrasena"];
 
 // Conectar a la base de datos (reemplaza con tus propios detalles de conexión)
 $conexion = new mysqli("localhost", "angel", "Kurokonoba95", "agro_tec");
@@ -13,14 +9,20 @@ if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 
-// Insertar los datos en la tabla de usuarios
-$insert_query = "INSERT INTO usuarios (nombre, correo, usuario, contrasena) VALUES ('$nombre', '$correo', '$usuario', '$contrasena')";
-
-if ($conexion->query($insert_query) === TRUE) {
-    echo "Registro exitoso";
-} else {
-    echo "Error al registrar: " . $conexion->error;
+if(isset($_REQUEST["btn-r-s"])){
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $usuario = $_POST["usuario"];
+    $contrasena = $_POST["contrasena"];
+    $insert_query = "INSERT INTO usuarios (nombre, correo, usuario, contrasena) VALUES ('$nombre', '$correo', '$usuario', '$contrasena')";
+    if ($conexion->query($insert_query) === TRUE) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error al registrar: " . $conexion->error;
+    }
 }
+// Insertar los datos en la tabla de usuarios
+
 
 // Cerrar la conexión
 $conexion->close();
