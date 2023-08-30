82ab0256f05bec481d381d1a87a20309706eb160
Leomi95 <leomills2004@gmail.com>
2023-08-30 18:45:14 -0300

    5

--- registro-php/index.php
+++ registro-php/index.php
@@ -1,12 +1,27 @@
-
 <?php
-// Conexión a la base de datos
-$conexion = new mysqli("localhost", "angel", "Kurokonoba95", "mi_base_de_datos") or die("error al conectart");
-if($conexion){
-    echo "conexion exitosa";
-    
-}
-   echo "hola mundo";
+// Obtener los datos del formulario de registro
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$usuario = $_POST["usuario"];
+$contrasena = $_POST["contrasena"];
+
+// Conectar a la base de datos (reemplaza con tus propios detalles de conexión)
+$conexion = new mysqli("localhost", "angel", "Kurokonoba95", "nombre_de_la_base");
+
 // Verificar la conexión
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
+// Insertar los datos en la tabla de usuarios
+$insert_query = "INSERT INTO usuarios (nombre, correo, usuario, contrasena) VALUES ('$nombre', '$correo', '$usuario', '$contrasena')";
+
+if ($conexion->query($insert_query) === TRUE) {
+    echo "Registro exitoso";
+} else {
+    echo "Error al registrar: " . $conexion->error;
+}
+
+// Cerrar la conexión
 $conexion->close();
 ?>
