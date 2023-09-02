fc065e5c7ff00428b07d3878f29e6f5dec033ebd
Leomi95 <leomills2004@gmail.com>
2023-09-02 13:31:24 -0300

    1

---
+++ registro-php/login.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_REQUEST["btn-s"])){
+    $correo = $_REQUEST["correo"];
+    $contraseña = $_REQUEST["contraseña"];
+  
+    $consulta = "SELECT nombre, correo, contraseña FROM usuarios WHERE correo = '$correo' AND contraseña = '$contraseña' ";
+    $response = mysqli_query($conex, $consulta);
+    if($response){
+      session_destroy();
+      session_start();
+      foreach ($response as $usuario ) {
+        $_SESSION["start"] = $usuario["nombre"];
+  
+      }
+      header("location:../home");
+    }else{
+      echo "usuario no encontrado";
+    }
+  
+  }
+?>
