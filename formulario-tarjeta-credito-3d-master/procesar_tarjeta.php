85bdeba6e831ede7f57fb8ac094b9566a0815cf1
Leomi95 <leomills2004@gmail.com>
2023-09-13 19:11:08 -0300

    actualizada123

--- formulario-tarjeta-credito-3d-master/procesar_tarjeta.php
+++ formulario-tarjeta-credito-3d-master/procesar_tarjeta.php
@@ -5,24 +5,27 @@
 if ($conexion->conexionect_error) {
     die("Error en la conexión a la base de datos: " . $conexion->conexionect_error);
 }
+echo "holaaa ";
 
-// Recibir datos del formulario
-$numero_tarjeta = $_POST['inputNumero'];
-$nombre = $_POST['inputNombre'];
-$mes_expiracion = $_POST['selectMes'];
-$year_expiracion = $_POST['selectYear'];
-$ccv = $_POST['inputCCV'];
-
-// SQL para insertar datos en la tabla (ajusta el nombre de la tabla y las columnas según tu estructura)
-$sql = "INSERT INTO tarjeta_credito (numero_tarjeta, nombre, mes_expiracion, year_expiracion, ccv) 
-        VALUES ('$numero_tarjeta', '$nombre', '$mes_expiracion', '$year_expiracion', '$ccv')";
-
-if ($conexion->query($sql) === TRUE) {
-    echo "Datos de tarjeta almacenados correctamente.";
-} else {
-    echo "Error al almacenar los datos de la tarjeta: " . $conexion->error;
+if(isset($_REQUEST["btn-t"])){
+    // Recibir datos del formulario
+    $numero_tarjeta = $_POST['inputNumero'];
+    $nombre = $_POST['inputNombre'];
+    $mes_expiracion = $_POST['selectMes'];
+    $year_expiracion = $_POST['selectYear'];
+    $ccv = $_POST['inputCCV'];
+    // SQL para insertar datos en la tabla (ajusta el nombre de la tabla y las columnas según tu estructura)
+    $sql = "INSERT INTO tarjeta_credito (numero_tarjeta, nombre, mes_expiracion, year_expiracion, ccv) 
+            VALUES ('$numero_tarjeta', '$nombre', '$mes_expiracion', '$year_expiracion', '$ccv')";
+    
+    if ($conexion->query($sql)) {
+        echo "Datos de tarjeta almacenados correctamente.";
+    } else {
+        echo "Error al almacenar los datos de la tarjeta: " . $conexion->error;
+    }
+    
+    // Cerrar la conexión
+    $conexion->close();
 }
 
-// Cerrar la conexión
-$conexion->close();
 ?>
