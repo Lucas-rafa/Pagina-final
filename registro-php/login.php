711f52f07a4c3d41399775234f2212130f1d289d
Leomi95 <leomills2004@gmail.com>
2023-09-13 12:19:32 -0300

    registrooooo

--- registro-php/login.php
+++ registro-php/login.php
@@ -1,29 +1,31 @@
 <?php
-// Conectar a la base de datos (reemplaza con tus propios detalles de conexión)
-$conexion = new mysqli("localhost", "angel", "Kurokonoba95", "agro_tec");
-
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
-}
-
-if(isset($_POST["btn-login"])){
-    $usuario = $_POST["usuario"];
+// Obtener los datos del formulario de inicio de sesión
+if (isset($_POST["btn-login"])) {
+    $correo = $_POST["correo"];
     $contrasena = $_POST["contrasena"];
-    
-    // Consulta para verificar las credenciales
-    $consulta = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contrasena = '$contrasena'";
-    $resultado = $conexion->query($consulta);
-    
-    if ($resultado->num_rows > 0) {
-        // Inicio de sesión exitoso, redirigir a la página de inicio o mostrar un mensaje de bienvenida
-        echo "Inicio de sesión exitoso. ¡Bienvenido, $usuario!";
-    } else {
-        // Las credenciales no coinciden, mostrar un mensaje de error
-        echo "Error: Credenciales incorrectas. Por favor, intenta nuevamente.";
-    }
-}
 
-// Cerrar la conexión
-$conexion->close();
+    // Conectar a la base de datos (reemplaza con tus propios detalles de conexión)
+    $conexion = new mysqli("localhost", "angel", "Kurokonoba95", "agro_tec");
+
+    // Verificar la conexión
+    if ($conexion->connect_error) {
+        die("Conexión fallida: " . $conexion->connect_error);
+    }
+
+    // Consultar la base de datos para verificar las credenciales del usuario
+    $consulta = "SELECT * FROM usuarios WHERE correo = '$correo' AND contrasena = '$contrasena'";
+    $resultado = $conexion->query($consulta);
+
+    if ($resultado->num_rows === 1) {
+        // El usuario se autenticó correctamente
+        echo "Inicio de sesión exitoso";
+        // Puedes redirigir al usuario a una página de bienvenida aquí
+    } else {
+        // Las credenciales son incorrectas
+        echo "Correo o contraseña incorrectos";
+    }
+
+    // Cerrar la conexión
+    $conexion->close();
+}
 ?>
