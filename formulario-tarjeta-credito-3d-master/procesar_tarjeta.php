a58776a4dad035fa3a9a97966fdc17cac7d35b01
Leomi95 <leomills2004@gmail.com>
2023-09-13 16:33:22 -0300

    actualizar

---
+++ formulario-tarjeta-credito-3d-master/procesar_tarjeta.php
@@ -0,0 +1,34 @@
+<?php
+// Conexión a la base de datos (reemplaza con tus propios valores)
+$servername = "localhost";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$database = "nombre_de_tu_base_de_datos";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Recibir datos del formulario
+$numero_tarjeta = $_POST['inputNumero'];
+$nombre = $_POST['inputNombre'];
+$mes_expiracion = $_POST['selectMes'];
+$year_expiracion = $_POST['selectYear'];
+$ccv = $_POST['inputCCV'];
+
+// SQL para insertar datos en la tabla (ajusta el nombre de la tabla y las columnas según tu estructura)
+$sql = "INSERT INTO tarjetas (numero_tarjeta, nombre, mes_expiracion, year_expiracion, ccv) 
+        VALUES ('$numero_tarjeta', '$nombre', '$mes_expiracion', '$year_expiracion', '$ccv')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Datos de tarjeta almacenados correctamente.";
+} else {
+    echo "Error al almacenar los datos de la tarjeta: " . $conn->error;
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
