05e911eec4640de6049ccf492cd422cc341d62ca
Leomi95 <leomills2004@gmail.com>
2023-09-13 12:02:44 -0300

    login 121

--- registro-php/login.php
+++ registro-php/login.php
@@ -1,22 +1,29 @@
 <?php
-if(isset($_REQUEST["btn-s"])){
-    $correo = $_REQUEST["correo"];
-    $contraseña = $_REQUEST["contrasena"];
-  
-    $consulta = "SELECT nombre, correo, contraseña FROM usuarios WHERE correo = '$correo' AND contrasena = '$contraseña' ";
-    $response = mysqli_query($conex, $consulta);
-    if($response){
-      session_destroy();
-      session_start();
-      foreach ($response as $usuario ) {
-        $_SESSION["start"] = $usuario["nombre"];
-  
-      }
-      header("location:../index");
-    }else{
-      echo "usuario no encontrado";
-    }
-  
-  }
+// Conectar a la base de datos (reemplaza con tus propios detalles de conexión)
+$conexion = new mysqli("localhost", "angel", "Kurokonoba95", "agro_tec");
 
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
+if(isset($_POST["btn-login"])){
+    $usuario = $_POST["usuario"];
+    $contrasena = $_POST["contrasena"];
+    
+    // Consulta para verificar las credenciales
+    $consulta = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contrasena = '$contrasena'";
+    $resultado = $conexion->query($consulta);
+    
+    if ($resultado->num_rows > 0) {
+        // Inicio de sesión exitoso, redirigir a la página de inicio o mostrar un mensaje de bienvenida
+        echo "Inicio de sesión exitoso. ¡Bienvenido, $usuario!";
+    } else {
+        // Las credenciales no coinciden, mostrar un mensaje de error
+        echo "Error: Credenciales incorrectas. Por favor, intenta nuevamente.";
+    }
+}
+
+// Cerrar la conexión
+$conexion->close();
 ?>
