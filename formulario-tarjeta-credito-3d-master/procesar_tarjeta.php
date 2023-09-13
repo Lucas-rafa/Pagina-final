098b97a72d5310793c2fa7c96e82864bb2cd4337
Leomi95 <leomills2004@gmail.com>
2023-09-13 20:11:41 -0300

    actualizacion

--- formulario-tarjeta-credito-3d-master/procesar_tarjeta.php
+++ formulario-tarjeta-credito-3d-master/procesar_tarjeta.php
@@ -9,11 +9,11 @@ echo "holaaa ";
 
 if(isset($_REQUEST["btn-t"])){
     // Recibir datos del formulario
-    $numero_tarjeta = $_POST['inputNumero'];
-    $nombre = $_POST['inputNombre'];
-    $mes_expiracion = $_POST['selectMes'];
-    $year_expiracion = $_POST['selectYear'];
-    $ccv = $_POST['inputCCV'];
+    $numero_tarjeta = $_POST['numero'];
+    $nombre = $_POST['nombre'];
+    $mes_expiracion = $_POST['mes'];
+    $year_expiracion = $_POST['año'];
+    $ccv = $_POST['cvv'];
     // SQL para insertar datos en la tabla (ajusta el nombre de la tabla y las columnas según tu estructura)
     $sql = "INSERT INTO tarjeta_credito (numero_tarjeta, nombre, mes_expiracion, year_expiracion, ccv) 
             VALUES ('$numero_tarjeta', '$nombre', '$mes_expiracion', '$year_expiracion', '$ccv')";
